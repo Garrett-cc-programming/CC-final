f0fede8d874dd956a8f4648b5935bed203598725
Garrett-cc-programming <garrettheichman@gmail.com>
2024-03-25 10:16:33 -0400

    yuh

--- Astronaut.java
+++ Astronaut.java
@@ -1,27 +1,22 @@
-// Create Astronaut class
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Astronaut{
-    // Attributes
+
     String name;
     StringBuilder results = new StringBuilder();
-    /* The Astronaut constructor accepts name,
-       assigns the local name to the attribute 
-       name, and creates a new results object. */
+
       public Astronaut(String name){
         this.name = name;
       }
-    /* The getName method accapts no arguments
-       and returns the name as a String. */
+
        public String getName(){
         return this.name;
        }
 
-    /* The performTask method accepts task, throws
-       IllegalArgumentExceptions if the task is empty
-       or adds the task to results if not */
        public void performTask(String task) throws InvalidTaskException{
       
-        // For invalid tasks: "Task cannot be empty."
-        // For valid tasks: "Task: [task]\n"
         if (task == null || task.isEmpty() || task == "") {
           throw new InvalidTaskException();
         }
@@ -34,21 +29,10 @@ public class Astronaut{
         }
        }
 
-    /* The getResults method accepts no arguments
-       and returns the results as a string. */
        public String getResults(){
         return results.toString();
        }
-    /* The isVaild method accepts task. It has two String
-       arrays to check for illegalTasks and impossibleTasks. If 
-       is a task is illegal, dangerous or impossible, this method
-       should return false. Otherwise, it should return true. */ 
-       // Invalid tasks: "Access other craft without authorization"
-                      // "Fire weapon"
-                      // "Turn off life support"
-                      // "Go for a jog"
-                      // "Go lie down in bed"
-                      // "Take a shower"
+
       private boolean isValid(String task) {
         String[] illegalTask = {"Access other craft without authorization", "Fire weapon", "Turn off life support", "Go for a jog", "Go lie down in bed", "Take a shower"};
         for (String illegalTasks : illegalTask) {
--- GFG.java
+++ GFG.java
@@ -35,37 +35,36 @@ public class GFG extends Frame implements WindowListener,ActionListener {
 
 		@Override
 		public void windowOpened(WindowEvent e) {
-			// TODO Auto-generated method stub
+
 			throw new UnsupportedOperationException("Unimplemented method 'windowOpened'");
 		}
 
 		@Override
 		public void windowClosed(WindowEvent e) {
-			// TODO Auto-generated method stub
 			throw new UnsupportedOperationException("Unimplemented method 'windowClosed'");
 		}
 
 		@Override
 		public void windowIconified(WindowEvent e) {
-			// TODO Auto-generated method stub
+
 			throw new UnsupportedOperationException("Unimplemented method 'windowIconified'");
 		}
 
 		@Override
 		public void windowDeiconified(WindowEvent e) {
-			// TODO Auto-generated method stub
+
 			throw new UnsupportedOperationException("Unimplemented method 'windowDeiconified'");
 		}
 
 		@Override
 		public void windowActivated(WindowEvent e) {
-			// TODO Auto-generated method stub
+
 			throw new UnsupportedOperationException("Unimplemented method 'windowActivated'");
 		}
 
 		@Override
 		public void windowDeactivated(WindowEvent e) {
-			// TODO Auto-generated method stub
+
 			throw new UnsupportedOperationException("Unimplemented method 'windowDeactivated'");
 		}
     }
